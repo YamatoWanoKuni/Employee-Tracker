cf360d4c56ee7c9a1037b7562c4d4ab99d5d436e
Christian <cmcarbajal05@gmail.com>
2024-11-24 19:44:04 -0500

    added tasks func. added view and add roles

--- src/server.ts
+++ src/server.ts
@@ -37,3 +37,85 @@ function viewDepartments() {
     }
   })
 };
+
+async function addRoles() {
+  try {
+    let answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Enter the name for the new role: '
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary for the new role: '
+      },
+      {
+        type: 'input',
+        name: 'department',
+        message: 'Enter the department the new role belongs to: '
+      }
+    ])
+    const fetchDepId = await pool.query('SELECT departments.id FROM departments WHERE departments.name = $1', [answers.department]);
+    const parseId = destructureEmployee(fetchDepId.rows[0]);
+    const result = await pool.query('INSERT INTO roles (title, salary, department) VALUES ($1, $2, $3)', [answers.role, answers.salary, parseId]);
+        if (result) {
+        console.log(`Role ${answers.role} added!`);
+        Tasks();
+      }
+    } catch (err) {
+  console.error(err);
+}
+};
+
+function viewRoles() {
+  pool.query('SELECT roles.title AS "Job Title", roles.id AS "Role ID", departments.name AS "Department", roles.salary AS "Salary" FROM roles JOIN departments ON roles.department = departments.id', (err: Error, result: QueryResult) => {
+    if (err) {
+      console.log(err);
+    } else if (result) {
+      console.table(result.rows);
+              
+            }
+          })
+};  
+
+function tasks() {
+  let exit: boolean = false;
+  inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: 'Select an action:',
+        choices: [
+          'View All Departments',
+          'Add a Department',
+          'Exit',
+        ],
+    },
+  ]) .then((answers) =>{
+      if (answers.action === 'Add a Department') {
+        addDepartments()
+        return;
+      }
+      else if (answers.action === 'View All Departments') {
+        viewDepartments();
+      }
+      else if (answers.action === 'Add a Role') {
+        addRoles()
+        return;
+      }
+      else if (answers.action === 'View All Roles') {
+        viewRoles()
+      }
+      else {
+        exit = true;
+        process.exit(0);
+
+      }
+      if (!exit){
+          tasks();
+      }
+      
+  })
+}
