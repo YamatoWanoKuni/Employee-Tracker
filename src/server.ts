641875e2982ce75a53bd5f5c6e7c45fd9507e94b
Christian <cmcarbajal05@gmail.com>
2024-11-24 21:27:23 -0500

    added view employees, add employees func

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ function addDepartments() {
       console.log(err);
     } else if (result) {
       console.log(`Department ${answers.department} added!`);
-      Tasks();
+      tasks();
     }
   })
 )
@@ -36,6 +36,11 @@ function viewDepartments() {
       console.table(result.rows);
     }
   })
+};
+
+function dEmployee(answers: any) {
+  const { id } = answers;
+  return id;
 };
 
 async function addRoles() {
@@ -58,11 +63,11 @@ async function addRoles() {
       }
     ])
     const fetchDepId = await pool.query('SELECT departments.id FROM departments WHERE departments.name = $1', [answers.department]);
-    const parseId = destructureEmployee(fetchDepId.rows[0]);
+    const parseId = dEmployee(fetchDepId.rows[0]);
     const result = await pool.query('INSERT INTO roles (title, salary, department) VALUES ($1, $2, $3)', [answers.role, answers.salary, parseId]);
         if (result) {
         console.log(`Role ${answers.role} added!`);
-        Tasks();
+        tasks();
       }
     } catch (err) {
   console.error(err);
@@ -80,6 +85,56 @@ function viewRoles() {
           })
 };  
 
+async function addEmployees() {
+  try {
+    let answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'Enter the first name of the employee: '
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'Enter the last name of the employee: '
+      },
+      {
+        type: 'input',
+        name: 'manager',
+        message: 'Enter the last name of the manager for the new employee: '
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Enter the job title of the employee: '
+      }
+    ]);
+    const manager_id = await pool.query('SELECT employees.id FROM employees WHERE employees.last_name = $1', [answers.manager]);
+    const parsedManID = dEmployee(manager_id.rows[0]);
+    const role_id = await pool.query('SELECT roles.id FROM roles WHERE roles.title = $1', [answers.role]);
+    const parsedRoleID = dEmployee(role_id.rows[0]);
+    const result = await pool.query('INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ($1, $2, $3, $4)', [answers.first_name,answers.last_name,parsedManID,parsedRoleID])
+        if (result) {
+        console.log(`Employee ${answers.first_name} ${answers.last_name} added!`);
+        tasks();
+      }
+    }
+    catch (err) {
+    console.error(err);
+  }
+};
+
+function viewEmployees() {
+  pool.query(`SELECT employees.first_name AS "First Name", employees.last_name AS "Last Name", roles.title AS "Job Title", roles.salary AS "Salary", departments.name AS "Department", manager.first_name || ' ' || manager.last_name AS "Manager"  FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.department = departments.id LEFT JOIN employees manager ON employees.manager_id = manager.id`, (err: Error, result: QueryResult) => {
+    if (err) {
+      console.log(err);
+    } else if (result) {
+      console.table(result.rows);
+              
+            }
+          })
+};
+
 function tasks() {
   let exit: boolean = false;
   inquirer.prompt([
@@ -90,6 +145,10 @@ function tasks() {
         choices: [
           'View All Departments',
           'Add a Department',
+          'Add a Role',
+          'View All Roles',
+          'View All Employees',
+          'Add an Employee',
           'Exit',
         ],
     },
@@ -103,11 +162,16 @@ function tasks() {
       }
       else if (answers.action === 'Add a Role') {
         addRoles()
-        return;
-      }
+        return;}
       else if (answers.action === 'View All Roles') {
         viewRoles()
       }
+      else if (answers.action === 'Add a Employee') {
+        addEmployees();
+        return;
+      }
+      else if (answers.action === 'View All Employees') {
+        viewEmployees() }
       else {
         exit = true;
         process.exit(0);
